4c45021e68083da0fc798c7bf3aa1e4176e12605
Syed Moiz <moiz.ali@tigerspike.com>
2019-05-05 20:17:12 +0400

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+// Import the react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from './header';
+import './css/style.css';
+import './css/newspaper.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create a react component
+const Home = () => {
+    return <Header />
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <Home/>,
+    document.querySelector('#root')
+)
